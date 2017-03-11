a3220e9a41bb1d43dc0794bd0bd1a10c0bf37684
Spencer Michael Moss <radio@another.computer>
2017-03-11 15:51:33 -0800

    Refactored Card.py and Deck.py

--- Hanabi/Deck.py
+++ Hanabi/Deck.py
@@ -1,45 +1,45 @@
-from Hanabi.Card import Card
+from Card import Card
+from Card import colors
+from Card import numbers
 from random import shuffle
 
 
 class Deck(object):
-    def __init__(self, multi_color):
-        self.deck = []
-        self.colors = {'Unknown': '[7;37;48m', 'Red': '[7;31;48m', 'Blue': '[7;34;48m', 'Green': '[7;32;48m',
-                       'Yellow': '[7;33;48m', 'White': '[7;30;48m'}
+    def __init__(self, difficulty):
+        self.cards = []
+        self.frequencies = [3, 2, 2, 2, 1]
+        self.excluded_colors = ["Unknown"]
 
-        if multi_color is True:
-            self.colors['Rainbow'] = '[0;35;48m'
+        if(difficulty == "Normal"):
+            self.excluded_colors.append("Rainbow")
+        elif(difficulty == "Difficult"):
+            self.frequencies = [1, 1, 1, 1, 1]
 
-        self.numbers = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
+        for color in colors.keys():
+            if color not in self.excluded_colors:
+                for number in numbers:
+                    for x in range(self.frequencies[number - 1]):
+                        self.cards.append(Card(color, number))
 
-        for color in self.colors.keys():
-            if color != 'Unknown':
-                for number in self.numbers:
-                    self.deck.append(Card(color, number))
+        shuffle(self.cards)
 
-        shuffle(self.deck)
-
-        self.cards_remaining = len(self.deck)
+        self.cards_remaining = len(self.cards)
 
     def draw(self):
-        drawn_card = self.deck[0]
-
-        del self.deck[0]
-
-        self.cards_remaining -= 1
+        drawn_card = self.cards.pop()
+        self.cards_remaining = len(self.cards)
 
         return drawn_card
 
 
-test = Deck(False)
+test = Deck("Normal")
 
-for card in test.deck:
-    print(card.__str__(test.colors))
+for card in test.cards:
+    print(card)
 print(test.cards_remaining)
 
 print('')
-print(test.deck[0].__str__(test.colors))
+print(test.cards[-1])
 card = test.draw()
-print(card.__str__(test.colors))
+print(card)
 print(test.cards_remaining)
