862b7c6552b0bf734f0e529c5230986264e554dc
cabal19421 <bleh@bleh.com>
2017-03-12 15:32:31 -0700

    stuff

--- Hanabi/Card.py
+++ Hanabi/Card.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # The deck is a randomized list of cards
 # 5 colors
 # 1 five, 2 fours/threes/twos, 3 ones per color
@@ -8,50 +10,49 @@
 
 # Cards have color, number, and clue status for both
 
-colors = {'Unknown': '[7;37;48m', 'Red': '[7;31;48m', 'Blue': '[7;34;48m', 
-          'Green': '[7;32;48m', 'Yellow': '[7;33;48m', 'White': '[7;30;48m',
-          'Rainbow': '[0;35;48m'}
-numbers = ['1', '2', '3', '4', '5']
+from constants import _COLORS, _NUMBERS
 
 
 class Card(object):
-    def __init__(self, color, number):
-        self.color = color
-        self.number = number
+  def __init__(self, color, number):
+    self.color = color
+    self.number = number
 
-        self.color_clue = False
-        self.number_clue = False
+    self.color_clue = False
+    self.number_clue = False
 
-    def __str__(self):
-        color_display = 'Unknown'
-        number_display = '?'
+  def __str__(self):
+      color_display = 'Unknown'
+      number_display = '?'
 
-        if self.color_clue is True:
-            color_display = self.color
+      if self.color_clue is True:
+          color_display = self.color
 
-        if self.number_clue is True:
-            number_display = self.number
+      if self.number_clue is True:
+          number_display = self.number
 
-        return '\x1b{}{}\x1b[0m'.format(colors[color_display], number_display)
+      return '\x1b{}{}\x1b[0m'.format(_COLORS[color_display],
+                                      number_display)
 
-    def __repr__(self):
-        color_display = 'Unknown'
-        number_display = '?'
+  def __repr__(self):
+      color_display = 'Unknown'
+      number_display = '?'
 
-        if self.color_clue is True:
-            color_display = self.color
+      if self.color_clue is True:
+          color_display = self.color
 
-        if self.number_clue is True:
-            number_display = self.number
+      if self.number_clue is True:
+          number_display = self.number
 
-        return '\x1b{}{}\x1b[0m'.format(colors[color_display], number_display)
-    
-    def clue_check(self, clue):
-        if clue == self.color:
-            self.color_clue = True
+      return '\x1b{}{}\x1b[0m'.format(_COLORS[color_display],
+                                      number_display)
 
-        if clue == self.number:
-            self.number_clue = True
+  def clue_check(self, clue):
+      if clue == self.color:
+          self.color_clue = True
+
+      if clue == self.number:
+          self.number_clue = True
 
 
 if __name__ == '__main__':
--- Hanabi/Player.py
+++ Hanabi/Player.py
@@ -1,44 +1,46 @@
+#!/usr/bin/python
+
 class Player(object):
 
-    def __init__(self):
+  def __init__(self):
 
-        self.name = None
+    self.name = None
 
-        self.hand = []
+    self.hand = []
 
-    def associate_client(self, client_name):
-        self.name = client_name
+  def associate_client(self, client_name):
+    self.name = client_name
 
-    def draw(self, drawn_card):
-        self.hand.insert(0, drawn_card)
+  def draw(self, drawn_card):
+    self.hand.insert(0, drawn_card)
 
-    def receive_clue(self, clue):
-        for card in self.hand:
-            if card.color == clue:
-                card.color_clue = True
+  def receive_clue(self, clue):
+    for card in self.hand:
+      if card.color == clue:
+        card.color_clue = True
 
-            elif card.number == clue:
-                card.number_clue = True
+      elif card.number == clue:
+        card.number_clue = True
 
 if __name__ == '__main__':
-    from Hanabi.Card import Card
+  from Card import Card
 
-    test = Player()
-    print(test.hand)
-    print('')
+  test = Player()
+  print(test.hand)
+  print('')
 
-    test.hand = [Card('Blue', '1'), Card('Red', '2'), Card('Green', '5'), Card('Yellow', '4'), Card('White', '5')]
-    print(test.hand)
-    print('')
+  test.hand = [Card('Blue', '1'), Card('Red', '2'), Card('Green', '5'), Card('Yellow', '4'), Card('White', '5')]
+  print(test.hand)
+  print('')
 
-    test.receive_clue('Blue')
-    print(test.hand)
-    print('')
+  test.receive_clue('Blue')
+  print(test.hand)
+  print('')
 
-    test.receive_clue('5')
-    print(test.hand)
-    print('')
+  test.receive_clue('5')
+  print(test.hand)
+  print('')
 
-    test.receive_clue('1')
-    print(test.hand)
+  test.receive_clue('1')
+  print(test.hand)
 
