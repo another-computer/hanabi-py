c0a7502b3d7d32b0c94c4cd138ee135e08d8a34f
cabal19421 <bleh@bleh.com>
2017-03-12 15:29:29 -0700

    * adding pythonic ways of doing things * and stuff

---
+++ Hanabi/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+_COLORS = {'Unknown': '[7;37;48m',
+           'Red': '[7;31;48m',
+           'Blue': '[7;34;48m', 
+           'Green': '[7;32;48m',
+           'Yellow': '[7;33;48m',
+           'White': '[7;30;48m',
+           'Rainbow': '[0;35;48m'}
+
+_NUMBERS = ['1', '2', '3', '4', '5']
